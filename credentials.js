ac0da834302dabed5cf14f4d0b12c7a072fe18d4
emre <emrealplion@gmail.com>
2019-03-19 00:57:08 +0300

    credentials removed

--- credentials.js
+++
@@ -1,4 +0,0 @@
-var API_KEY =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHAiLCJhcHAiOjExMSwiYnVzIjpudWxsLCJjb21wIjpudWxsLCJzY3J0IjoiUlYwMUM4In0.kfDjLjkMS-78yyHoprBLJwIeJgNSY5UVeJwK5woyxBU"; // <--- Change to your API key.
-var XBUSINESS_HID = "_d567ag7a"; // <--- Change to your X-Business hid.
-var BRANCH_HID = "_5g375g79"; // <--- Change to your X-Business hid.
