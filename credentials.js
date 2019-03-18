ba2dca823d1fee6c3f10af47e701adf1a5d79df0
emre <emrealplion@gmail.com>
2019-03-19 00:52:38 +0300

    gitignore added

---
+++ credentials.js
@@ -0,0 +1,4 @@
+var API_KEY =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcHAiLCJhcHAiOjExMSwiYnVzIjpudWxsLCJjb21wIjpudWxsLCJzY3J0IjoiUlYwMUM4In0.kfDjLjkMS-78yyHoprBLJwIeJgNSY5UVeJwK5woyxBU"; // <--- Change to your API key.
+var XBUSINESS_HID = "_d567ag7a"; // <--- Change to your X-Business hid.
+var BRANCH_HID = "_5g375g79"; // <--- Change to your X-Business hid.
