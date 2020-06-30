da44b6b5ea52ed2952d883a44ce3c29bc2b14674
emre <emrealplion@gmail.com>
2020-06-30 11:33:32 +0300

    new api

--- test/test.js
+++ test/test.js
@@ -5,11 +5,14 @@ const chai = require("chai");
 const expect = chai.expect;
 const should = chai.should();
 
-const API_KEY = "XYZ"; // <--- Change to your API key.
+const API_KEY = "8d90d06e-bb3d-43f1-8407-xxxxxx"; // <--- Change to your API key.
 const ENVIRONMENT = "staging"; // <--- Change the environment you want to test.
 
-const nodeFoodics = new NodeFoodics(API_KEY, ENVIRONMENT);
+const nodeFoodics = new NodeFoodics(null, ENVIRONMENT);
 
 describe("node-foodics initialize", () => {
-  it("should return branches", async () => {});
+  it("should return who you are", async () => {
+    // const whoami = await nodeFoodics.authentication(code, id, secret, redirect)
+    // console.log(whoami);
+  });
 });
